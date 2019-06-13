8172eee8c7e9cc6fbfad1bcae3b6cce862bedd22
hoduchieu01 <hoduchieu01@gmail.com>
2019-06-13 15:06:20 +0700

    add countdown

---
+++ js/countdown.js
@@ -0,0 +1,31 @@
+// Set the date we're counting down to
+var countDownDate = new Date("July 13, 2019 09:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("count_down").innerHTML =
+        days + "<span>" + " Ngày " + "</span>" +
+        hours + "<span>" + " Giờ " + "</span>" +
+        minutes + "<span>" + " Phút " + "</span>" +
+        seconds + "<span>" + " Giây " + "</span>";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("count_down").innerHTML = "Welcome to SheCodesHackathon 2019";
+    }
+}, 1000);
